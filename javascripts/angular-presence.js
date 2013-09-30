789ebc3f120a93209162ef461ea625a90c17ae36
Timo Brandes <kaateebee@gmail.com>
2013-09-30 22:22:19 +0200

    Created exmaples

---
+++ javascripts/angular-presence.js
@@ -0,0 +1,200 @@
+(function() {
+  'use strict';
+  angular.module('presence', [])
+
+  .directive('presence', function($presence, types) {
+    function getTypeNames(param) {
+      if (!param) {
+        return types.getAllTypeNames();
+      } else {
+        return param.split(' ');
+      }
+    }
+
+    return {
+      restrict: 'A',
+      link: function(scope, element, attrs) {
+        angular.forEach(getTypeNames(attrs.presence), function(typeName) {
+          var type = types.get(typeName);
+          element.on(type.events, function() {
+            $presence.registerAction(type.name);
+          });
+        });
+      }
+    };
+  })
+
+  .factory('types', function() {
+    return {
+      MOUSE: { name: 'MOUSE', events: 'click mousedown mouseup mousemove' },
+      KEYBOARD: { name: 'KEYBOARD', events: 'keypress keydown keyup' },
+      TOUCH: { name: 'TOUCH', events: 'touchstart touchmove touchend touchenter touchleave touchcancel'},
+      getAllTypeNames: function() {
+        return ['MOUSE', 'KEYBOARD', 'TOUCH'];
+      },
+      get: function(type) {
+        type = type.toUpperCase();
+        if (this[type]) {
+          return this[type];
+        }
+        throw new Error("Unknown type for monitoring presence: " + type);
+      }
+    };
+
+  })
+
+  .factory('$presence', function ($timeout, $q, orderByFilter, types) {
+    var entryState = {},
+        states,
+        initialStateId = 0,
+        currentStateId,
+        timer,
+        callbacksStateLeave = {},
+        callbacksStateEnter = {},
+        callbacksStateChange = [];
+
+    function init(statesInput) {
+
+      function objectify() {
+        angular.forEach(statesInput, function(state, key) {
+          if (!angular.isObject(state)) {
+            statesInput[key] = state = { enter: state };
+          }
+          state.name = key;
+          state.enter = state.enter || 0;
+        });
+        return statesInput;
+      }
+
+      function sortStates() {
+        var statesArray = [];
+        angular.forEach(statesInput, function(state) {
+          statesArray.push(state);
+        });
+        return orderByFilter(statesArray, "enter");
+      }
+
+      function extendStates() {
+        angular.forEach(states, function(state, id) {
+          state.id = id;
+          state.onEnter = function(fn) {
+            onStateEnter(id, fn);
+          };
+          state.onLeave = function(fn) {
+            onStateLeave(id, fn);
+          };
+        });
+      }
+
+      function extendStatesInput() {
+        statesInput.onChange = function(fn) {
+          onStateChange(fn);
+        };
+        statesInput.getCurrent = function() {
+          return getCurrentState();
+        };
+      }
+
+      function initInternalStructures() {
+        angular.forEach(states, function(state, id) {
+          function setEntryState(type) {
+            if (state.accept.toUpperCase().indexOf(type) === -1) {
+              entryState[type] = id+1;
+            }
+          }
+
+          if (state.initial === true) {
+            initialStateId = id;
+          }
+          if (state.accept) {
+            angular.forEach(types.getAllTypeNames(), setEntryState);
+          }
+        });
+      }
+
+      statesInput = objectify();
+      states = sortStates();
+      extendStates();
+      extendStatesInput();
+      initInternalStructures();
+      changeState(initialStateId);
+
+      return statesInput;
+    }
+
+    function changeState(newStateId) {
+      var oldStateId = currentStateId;
+
+      if (states[oldStateId]) {
+        states[oldStateId].leftOn = new Date();
+        states[oldStateId].active = false;
+      }
+      states[newStateId].active = true;
+      states[newStateId].enteredOn = new Date();
+      states[newStateId].enteredFrom = states[oldStateId] ? states[oldStateId].name : undefined;
+
+      currentStateId = newStateId;
+
+      $timeout(function() {
+        notify(callbacksStateLeave[oldStateId]);
+        notify(callbacksStateEnter[newStateId]);
+        notify(callbacksStateChange);
+      });
+
+      restartTimer();
+    }
+
+    function changeStateToNext() {
+      changeState(currentStateId + 1);
+    }   
+
+    function notify(callbacks) {
+      if (callbacks) {
+        for (var i = 0; i < callbacks.length; i++) {
+          callbacks[i](states[currentStateId]);
+        }
+      }
+    }
+  
+    function restartTimer() {
+      var nextState = currentStateId+1;
+      if (states[nextState]) {
+        $timeout.cancel(timer);
+        timer = $timeout(changeStateToNext, states[nextState].enter - states[currentStateId].enter);
+      }
+    }
+  
+    function registerAction(type) {
+      if (!states) {
+        return;
+      }
+      var targetStateId = entryState[type] || 0;
+      if (targetStateId < currentStateId) {
+        changeState(targetStateId);
+      } else if (targetStateId === currentStateId) {
+        restartTimer();
+      }
+    }
+
+    function onStateChange(fn) {
+      callbacksStateChange.push(fn);
+    }
+
+    function onStateEnter(state, fn) {
+      (callbacksStateEnter[state] || (callbacksStateEnter[state] = [])).push(fn);
+    }
+
+    function onStateLeave(state, fn) {
+      (callbacksStateLeave[state] || (callbacksStateLeave[state] = [])).push(fn);
+    }
+
+    function getCurrentState() {
+      return states[currentStateId];
+    }
+
+    return {
+      init: init,
+      registerAction: registerAction
+    };
+  });
+}());
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,39 @@
-console.log('This would be the main JS file.');
+angular.module('presence-example-one', ['presence'])
+.factory('states', function($presence) {
+  return $presence.init({
+    ACTIVE : { enter: 0, text: "I know you're there!" },
+    INACTIVE : {enter: 1000, text: "Are you there?" }
+  });
+})
+.controller('ctrl', function($scope, states) {
+  $scope.states = states;
+});
+
+angular.module('presence-example-two', ['presence'])
+.factory('states', function($presence) {
+  return $presence.init({
+    TYPING : {
+      accept: "kEYBOARD", text: "Hey, you are typing!"
+    },
+    IDLE : {
+      enter: 2000, initial: true, text: "I know you're there, why dont you type something?",
+    },
+    SHORTAWAY : {
+	  enter: 5000, text: "Ok, i think you're gone."
+	},
+	LONGAWAY : {
+	  enter: 10000, text: "You're definetly gone... but you should type something."
+	}
+  });
+})
+.controller('ctrl', function($scope, states) {
+  $scope.states = states;
+
+  states.onChange(function(state) {
+    $scope.text = state.text;
+  });
+  
+  states.LONGAWAY.onLeave(function() {
+    alert("Hey, you are back!");
+  });
+});
